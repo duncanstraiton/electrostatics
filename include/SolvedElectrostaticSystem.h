45361f1d7b2a4e03124119529b956901e9f28721
Duncan Straiton <duncan@d1s.co.uk>
2016-01-16 15:06:54 +0000

    Added SolvedElectrostaticSystem class
    Added SolvedElectrostaticSystem class extending ElectrostaticSystem.
    Currently the exact same as ElectrostaticSystem but plan on adding
    methods that would not make sense for an unsolved system such as
    plotting, finding equipotential lines etc

---
+++ include/SolvedElectrostaticSystem.h
@@ -0,0 +1,27 @@
+/**
+ * A class to represent a solved electrostatic system.
+ *
+ * Inherits from ElectrostaticSystem, adding methods that wouldn't
+ * make sense for an unsolved system, eg plotting, finding equipotentials etc.
+ *
+ * This extra functionallity has not yet been implemented, so it is currently
+ * the exact same as ElectrostaticSystem.
+ */
+
+#ifndef SOLVEDELECTROSTATICSYSTEM_H
+#define SOLVEDELECTROSTATICSYSTEM_H
+
+#include <Eigen/Dense>
+#include "ElectrostaticSystem.h"
+
+namespace electrostatics {
+
+class SolvedElectrostaticSystem : public ElectrostaticSystem{
+    public:
+        /* Constructors. */
+        SolvedElectrostaticSystem(Eigen::MatrixXd &potentials, int iMin, int jMin);
+        SolvedElectrostaticSystem(int iMin, int iMax, int jMin, int jMax);
+};
+
+} // namespace electrostatics
+#endif
---
+++ src/SolvedElectrostaticSystem.cpp
@@ -0,0 +1,19 @@
+#include <Eigen/Dense>
+#include <stdexcept>
+#include "ElectrostaticSystem.h"
+#include "SolvedElectrostaticSystem.h"
+
+namespace electrostatics {
+
+/* Constructors */
+
+SolvedElectrostaticSystem::SolvedElectrostaticSystem(
+        Eigen::MatrixXd &potentials, int iMin, int jMin) :
+    ElectrostaticSystem(potentials, iMin, jMin) {
+}
+
+SolvedElectrostaticSystem::SolvedElectrostaticSystem(int iMin, int iMax, int jMin, int jMax) :
+    ElectrostaticSystem(iMin, iMax, jMin, jMax) {
+}
+
+} // namespace electrostatics
