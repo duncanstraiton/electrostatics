1589ec8c166dcb9ca485fbf91f6149d0bbbe496a
Duncan Straiton <duncan@d1s.co.uk>
2016-01-16 01:08:15 +0000

    Added ElectrostaticSystem class and implementation
    Added an ElectrostaticSystem class to represent a generic electrostatic
    system

---
+++ include/ElectrostaticSystem.h
@@ -0,0 +1,77 @@
+/**
+ * A class to represent an electrostatic system.
+ *
+ * The system is represented as a grid with columns i and rows j.
+ * i increases from left to right and j increase from bottom to top.
+ * i and j do not necessarally start at zero. eg iMin could be negative.
+ * Each position is assigned a position number, k, starting from the
+ * bottom left as shown:
+ *
+ * 8  9 10 11
+ * 4  5  6  7
+ * 0  1  2  3
+ *
+ * Each position in the grid corresponds to the electric potential at that
+ * point.
+ *
+ * The system is stored as a (dense) matrix from the eigen library.
+ * Because eigen matrices are indexed by (row, column), eg:
+ * (row 0, col 0)    (row 0, col 1)    (row 0, col 2)
+ * (row 1, col 0)    (row 1, col 1)    (row 1, col 2)
+ *
+ * and the grid is indexed (like an x-y system) by (column, row), eg:
+ * (col 0, row 1)    (col 1, row 1)    (col 2, row 1)
+ * (col 0, row 0)    (col 1, row 0)    (col 2, row 0)
+ *
+ * to get from the matrix representaion to the grid representation the matrix 
+ * must be transposed and then fliped vertically. 
+ */
+
+#ifndef ELECTROSTATICSYSTEM_H
+#define ELECTROSTATICSYSTEM_H
+
+#include <Eigen/Dense>
+
+namespace electrostatics{
+
+class ElectrostaticSystem {
+    protected:
+        int iMin, iMax, jMin, jMax;
+        long kMax;  // kMax = (iMax - iMin) * (jMax - jMin)  -- It may be very large
+        Eigen::MatrixXd potentials;
+
+    public:
+        /* Constructors. */
+        ElectrostaticSystem(int iMin, int iMax, int jMin, int jMax);
+        ElectrostaticSystem(Eigen::MatrixXd &potentials, int iMin, int jMin);
+
+
+        /* Get min/max values for i, j, k. kMin is alway 0. */
+        int getIMin() const { return iMin; }
+        int getIMax() const { return iMax; }
+        int getJMin() const { return jMin; }
+        int getJMax() const { return jMax; }
+        long getKMax() const { return kMax; }
+
+        /* Get the potential at position (i, j) or (k).*/
+        double getPotentialIJ(int i, int j) const;
+        double getPotentialK(long k) const;
+
+
+        /* Set the potential at position (i, j) or (k).*/
+        void setPotentialIJ(int i, int j, double potential);
+        void setPotentialK(long k, double potential);
+
+        /* Get the potential matrix. */
+        Eigen::MatrixXd* getPotentials();
+
+        /* Convert between (i, j) coordinates and (k) positions. */
+        long ij2k(int i, int j) const;
+        int* k2ij(long k) const;
+
+        /* Print out the system potentials in the grid form described above/ */
+        void print() const;
+};
+
+} // namespace electrostatics
+#endif
---
+++ src/ElectrostaticSystem.cpp
@@ -0,0 +1,76 @@
+#include <Eigen/Dense>
+#include <stdexcept>
+#include <iostream>
+#include "ElectrostaticSystem.h"
+
+namespace electrostatics {
+
+/* Constructors */
+
+ElectrostaticSystem::ElectrostaticSystem(Eigen::MatrixXd &potentials, int iMin, int jMin) :
+    iMin(iMin), jMin(jMin), potentials(potentials) {
+        iMax = iMin + potentials.cols() - 1;
+        jMax = jMin + potentials.rows() - 1;
+        kMax = ((iMax-iMin+1) * (jMax-jMin+1)) - 1;
+}
+
+ElectrostaticSystem::ElectrostaticSystem(int iMin, int iMax, int jMin, int jMax) :
+    iMin(iMin), iMax(iMax), jMin(jMin), jMax(jMax) {
+        potentials = Eigen::MatrixXd::Zero(iMax-iMin+1, jMax-jMin+1);
+        kMax = ((iMax-iMin+1) * (jMax-jMin+1)) - 1;
+}
+
+
+/* Methods */
+
+double ElectrostaticSystem::getPotentialIJ(int i, int j) const {
+    if(i>iMax || i<iMin || j>jMax || j<jMin) throw std::out_of_range(
+            "Error: Trying to get element out of range!");
+    return potentials(i-iMin, j-jMin);
+}
+double ElectrostaticSystem::getPotentialK(long k) const {
+    if(k>kMax || k<0) throw std::out_of_range("Error: Trying to get element out of range!");
+    int* ij = k2ij(k);
+    return potentials(ij[0]-iMin, ij[1]-jMin);
+}
+
+void ElectrostaticSystem::setPotentialIJ(int i, int j, double potential) {
+    if(i>iMax || i<iMin || j>jMax || j<jMin) throw std::out_of_range(
+            "Error: Trying to set element out of range!");
+    potentials(i-iMin, j-jMin) = potential;
+}
+void ElectrostaticSystem::setPotentialK(long k, double potential) {
+    if(k>kMax || k<0) throw std::out_of_range("Error: Trying to set element out of range!");
+    int* ij = k2ij(k);
+    potentials(ij[0]-iMin, ij[1]-jMin) = potential;
+}
+
+Eigen::MatrixXd* ElectrostaticSystem::getPotentials() { return &potentials; }
+
+long ElectrostaticSystem::ij2k(int i, int j) const {
+    if(i>iMax || i<iMin || j>jMax || j<jMin) throw std::out_of_range(
+            "Error: Trying to convert to position (k) out of range!");
+    i = i - iMin;
+    j = j - jMin;
+    return i + j*(iMax-iMin+1);
+}
+
+int* ElectrostaticSystem::k2ij(long k) const {
+    if(k>kMax || k<0) throw std::out_of_range(
+            "Error: Trying to convert to position (k)  out of range!");
+    int* ij = new int[2];
+    int j = k / (iMax-iMin+1);
+    int i = k - j*(iMax-iMin+1);
+    ij[0] = i + iMin;
+    ij[1] = j + jMin;
+    return ij;
+}
+
+/* To convert from the matrix representaion to the grid representation,
+ * transpose the matrix and reverse each column
+ */
+void ElectrostaticSystem::print() const {
+    std::cout << potentials.transpose().colwise().reverse();
+}
+
+} // namespace electrostatics
