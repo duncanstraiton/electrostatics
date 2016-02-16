f4e56c6a197d955799a8c10e375e94eaf5965cf5
Duncan Straiton <duncan@d1s.co.uk>
2016-02-16 01:22:21 +0000

    Fixed problems with iterative finite diff

--- src/finiteDifferenceSolveIterative.cpp
+++ src/finiteDifferenceSolveIterative.cpp
@@ -2,6 +2,7 @@
 
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
+#include <iostream>
 #include "finiteDifferenceSolve.h"
 #include "SolvedElectrostaticSystem.h"
 #include "UnsolvedElectrostaticSystem.h"
@@ -15,33 +16,72 @@ namespace electrostatics {
 void finiteDifferenceSolve(UnsolvedElectrostaticSystem &unsolvedSystem,
         SolvedElectrostaticSystem &solvedSystem, int maxIterations) {
 
+    int iMin = unsolvedSystem.getIMin();
+    int iMax = unsolvedSystem.getIMax();
+    int jMin = unsolvedSystem.getJMin();
+    int jMax = unsolvedSystem.getJMax();
+
+    // Copy boundary conditions over to solvedSystem
+    for(int i=iMin; i<=iMax; i++) {
+        for(int j=jMin; j<=jMax; j++) {
+            solvedSystem.setPotentialIJ(i, j, unsolvedSystem.getPotentialIJ(i, j));
+        }
+    }
+
     // Loop for the required number of iterations
     for(int iter=1; iter<=maxIterations; iter++) {
         // Loop over all the points in the system
-        for(int i=unsolvedSystem.getIMin();i<=unsolvedSystem.getIMax();i++) {
-            for(int j=unsolvedSystem.getJMin();j<=unsolvedSystem.getJMax();j++) {
+        for(int i=iMin; i<=iMax; i++) {
+            for(int j=jMin; j<=jMax; j++) {
 
-                // If the point is a boundary condition, leave it along
-                if (unsolvedSystem.isBoundaryConditionIJ(i,j)) continue;
+                // If the point is a boundary condition, leave it alone
+                if(unsolvedSystem.isBoundaryConditionIJ(i, j)) {
+                    continue;
+                }
+
+                int surroundingPoints = 0;  // Number of points surrounding (i, j)
+                double sum = 0;             // Sum of potentials around (i, j)
 
                 // If on an odd iteration, go from unsolved to solved
                 if(iter%2 == 1) {
-                    double sum=0;
-                    if(i<unsolvedSystem.getIMax()) sum += unsolvedSystem.getPotentialIJ(i+1, j);
-                    if(i>unsolvedSystem.getIMin()) sum += unsolvedSystem.getPotentialIJ(i-1, j);
-                    if(j<unsolvedSystem.getJMax()) sum += unsolvedSystem.getPotentialIJ(i, j+1);
-                    if(j>unsolvedSystem.getJMin()) sum += unsolvedSystem.getPotentialIJ(i, j-1);
-                    solvedSystem.setPotentialIJ(i, j, sum/4);
+                    if(i<iMax) {
+                        surroundingPoints += 1;
+                        sum += unsolvedSystem.getPotentialIJ(i+1, j);
+                    }
+                    if(i>iMin) {
+                        surroundingPoints += 1;
+                        sum += unsolvedSystem.getPotentialIJ(i-1, j);
+                    }
+                    if(j<jMax) {
+                        surroundingPoints += 1;
+                        sum += unsolvedSystem.getPotentialIJ(i, j+1);
+                    }
+                    if(j>jMin) {
+                        surroundingPoints += 1;
+                        sum += unsolvedSystem.getPotentialIJ(i, j-1);
+                    }
+                    solvedSystem.setPotentialIJ(i, j, sum/surroundingPoints);
                 }
 
                 // Otherwise for even iteration, go from solved to unsolved
                 else {
-                    double sum=0;
-                    if(i<solvedSystem.getIMax()) sum += solvedSystem.getPotentialIJ(i+1, j);
-                    if(i>solvedSystem.getIMin()) sum += solvedSystem.getPotentialIJ(i-1, j);
-                    if(j<solvedSystem.getJMax()) sum += solvedSystem.getPotentialIJ(i, j+1);
-                    if(j>solvedSystem.getJMin()) sum += solvedSystem.getPotentialIJ(i, j-1);
-                    unsolvedSystem.setPotentialIJ(i,j, sum/4);
+                    if(i<iMax) {
+                        surroundingPoints += 1;
+                        sum += solvedSystem.getPotentialIJ(i+1, j);
+                    }
+                    if(i>iMin) {
+                        surroundingPoints += 1;
+                        sum += solvedSystem.getPotentialIJ(i-1, j);
+                    }
+                    if(j<jMax) {
+                        surroundingPoints += 1;
+                        sum += solvedSystem.getPotentialIJ(i, j+1);
+                    }
+                    if(j>jMin) {
+                        surroundingPoints += 1;
+                        sum += solvedSystem.getPotentialIJ(i, j-1);
+                    }
+                    unsolvedSystem.setPotentialIJ(i, j, sum/surroundingPoints);
                 }
             }   
         }
